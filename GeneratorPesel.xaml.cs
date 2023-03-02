c87ee8c4927716a7d0a7ea70977dac88200c35bc
Patryk Marchewka <44062662+MoD3223@users.noreply.github.com>
2023-03-02 10:47:39 +0100

    Aktualizacja na zajęciach

---
+++ GeneratorPesel.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AplikacjaPesel
+{
+    /// <summary>
+    /// Logika interakcji dla klasy GeneratorPesel.xaml
+    /// </summary>
+    public partial class GeneratorPesel : Window
+    {
+        public GeneratorPesel()
+        {
+            InitializeComponent();
+        }
+
+        private void GeneratorPESEL_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+   
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -57,6 +57,12 @@ namespace AplikacjaPesel
             return true;
         }
 
+        private void WykrytoBlad()
+        {
+            txtBlockCalosc.Visibility = Visibility.Visible;
+            txtBlockCalosc.Foreground = new SolidColorBrush(Colors.Red);
+            txtBlockCalosc.Text = "Znaleziono bledy! Sprawdz swoj PESEL";
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -69,10 +75,50 @@ namespace AplikacjaPesel
             tekst = txtBoxPESEL.Text.ToString();
             if (PoprawnyPesel(tekst))
             {
+                for (int i = 0; i < 11; i++)
+                {
+                    PESELByte[i] = byte.Parse(tekst.Substring(i, 1));
+                }
+                if (!checkDay() || !checkMonth())
+                {
+                    WykrytoBlad();
 
-            }
-            else
-            {
+                }
+                if (getSex() == "---")
+                {
+                    WykrytoBlad();
+                }
+                if (!checkSum())
+                {
+                    WykrytoBlad();
+                }
+
+
+                if (checkDay() & checkMonth() & getSex() != "---" & checkSum())
+                {
+
+                    txtBlockRok.Visibility = Visibility.Visible;
+                    txtBlockRok.Foreground = new SolidColorBrush(Colors.Green);
+                    txtBlockRok.Text = $"Twoja data urodzenia to {getBirthYear()}-{getBirthMonth()}-{getBirthDay()} (Format yyyy-mm-dd)";
+
+
+                    txtBlockGender.Visibility = Visibility.Visible;
+                    txtBlockGender.Foreground = new SolidColorBrush(Colors.Green);
+                    txtBlockGender.Text = $"Twoja plec to {getSex()}";
+
+                    txtBlockSuma.Visibility = Visibility.Visible;
+                    txtBlockSuma.Foreground = new SolidColorBrush(Colors.Green);
+                    txtBlockSuma.Text = "Poprawna suma kontrolna";
+
+
+                    txtBlockCalosc.Visibility = Visibility.Visible;
+                    txtBlockCalosc.Foreground = new SolidColorBrush(Colors.Green);
+                    txtBlockCalosc.Text = "Wpisano poprawny PESEL";
+                }
+                else
+                {
+                    WykrytoBlad();
+                }
 
             }
 
@@ -80,7 +126,7 @@ namespace AplikacjaPesel
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void TextBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -91,6 +137,15 @@ namespace AplikacjaPesel
         private void txtBoxPESEL_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+            if (txtBlockRok != null & txtBlockGender != null & txtBlockSuma != null & txtBlockCalosc != null)
+            {
+
+                txtBlockRok.Visibility = Visibility.Hidden;
+                txtBlockGender.Visibility = Visibility.Hidden;
+                txtBlockSuma.Visibility = Visibility.Hidden;
+                txtBlockCalosc.Visibility = Visibility.Hidden;
+
+            }
             if (peselCheckTXTBlock != null)
             {
                 UsunLitery();
@@ -145,6 +200,130 @@ namespace AplikacjaPesel
                 year += 2200;
             }
             return year;
+        }
+
+        public int getBirthMonth()
+        {
+            int month;
+            month = 10 * PESELByte[2];
+            month += PESELByte[3];
+            if (month > 80 && month < 93)
+            {
+                month -= 80;
+            }
+            else if (month > 20 && month < 33)
+            {
+                month -= 20;
+            }
+            else if (month > 40 && month < 53)
+            {
+                month -= 40;
+            }
+            else if (month > 60 && month < 73)
+            {
+                month -= 60;
+            }
+            return month;
+        }
+        public int getBirthDay()
+        {
+            int day;
+            day = 10 * PESELByte[4];
+            day += PESELByte[5];
+            return day;
+        }
+
+        public String getSex()
+        {
+                if (PESELByte[9] % 2 == 1)
+                {
+                    return "Mezczyzna";
+                }
+                else if (PESELByte[9] % 2 == 0)
+                {
+                    return "Kobieta";
+                }
+                else
+                {
+                return "---";
+                }
+        }
+
+        private bool checkSum()
+        {
+            int sum = 1 * PESELByte[0] +
+            3 * PESELByte[1] +
+            7 * PESELByte[2] +
+            9 * PESELByte[3] +
+            1 * PESELByte[4] +
+            3 * PESELByte[5] +
+            7 * PESELByte[6] +
+            9 * PESELByte[7] +
+            1 * PESELByte[8] +
+            3 * PESELByte[9];
+            sum %= 10;
+            sum = 10 - sum;
+            sum %= 10;
+
+            if (sum == PESELByte[10])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool checkMonth()
+        {
+            int month = getBirthMonth();
+            int day = getBirthDay();
+            if (month > 0 && month < 13)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool checkDay()
+        {
+            int year = getBirthYear();
+            int month = getBirthMonth();
+            int day = getBirthDay();
+            if ((day > 0 && day < 32) &&
+            (month == 1 || month == 3 || month == 5 ||
+            month == 7 || month == 8 || month == 10 ||
+            month == 12))
+            {
+                return true;
+            }
+            else if ((day > 0 && day < 31) &&
+            (month == 4 || month == 6 || month == 9 ||
+            month == 11))
+            {
+                return true;
+            }
+            else if ((day > 0 && day < 30 && leapYear(year)) ||
+            (day > 0 && day < 29 && !leapYear(year)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool leapYear(int year)
+        {
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                return true;
+            else
+                return false;
         }
     }
 }
